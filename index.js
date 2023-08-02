957a7394f546316e346646e450924e77b9f5fba4
OneFourteen Studio <onefourteenstudio114@gmail.com>
2023-08-02 15:13:05 -0400

    refactoring js

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
 const convertBtn = document.querySelector('#convert-btn')
-let userInput = document.querySelector('#user-input')
-const length = document.querySelector('#length')
-const volume = document.querySelector('#volume')
-const mass = document.querySelector('#mass')
+const conversions = document.querySelectorAll('.conversion-p')
+const length = document.getElementById('length-text')
+const volume = document.getElementById('volume-text')
+const mass = document.getElementById('mass-text')
+const mToFt = 3.281
+const lToG =  3.785
+const kgToLb = 2.205
 let leftConvert
 let rightConvert
-let measurement1
-let measurement2
-let conversion = `${userInput} ${measurement1} = ${leftConvert} ${measurement2} | ${userInput} ${measurement2} = ${rightConvert} ${measurement1} `
+let userInput = document.querySelector('#user-input')
+let conversion 
 
-// reusable variables for conversion measurements
-function measurement(section) {
-    if(section === 'length') {
-        measurement1 = "meters"
-        measurement2 = "feet"
-    } else if(section === 'volume') {
-        measurement1 = "liters"
-        measurement2 = "gallons"
-    }else if(section === 'mass') {
-        measurement1 = "kilograms"
-        measurement2 = "pounds"
-    }
-    
-    return measurement1, measurement2
+function getLength() {
+    leftConvert = (userInput.value * mToFt).toFixed(3)
+    rightConvert = (userInput.value / mToFt).toFixed(3)
+    conversion = `${userInput.value} meters = ${leftConvert} feet | ${userInput.value} feet = ${rightConvert} meters`
+    length.textContent = conversion
 }
 
-// create p tag and add class under length, volume, mass divs
-function createP () {
-    let convertP = document.createElement("p")
-    convertP.classList.add('conversion-p')
-    return convertP
+function getVolume() {
+    leftConvert = (userInput.value * lToG).toFixed(3)
+    rightConvert = (userInput.value / lToG).toFixed(3)
+    conversion = `${userInput.value} liters = ${leftConvert} gallons | ${userInput.value} gallons = ${rightConvert} liters`
+    volume.textContent = conversion
 }
 
-
-function lengthConvert() {
-    pTag = createP()
-    leftConvert = userInput * 3.281
-    rightConvert = userInput / 3.281
-    measurement("length")
-    length.appendChild(pTag)
-    pTag.textContent = conversion
-
+function getMass() {
+    leftConvert = (userInput.value * kgToLb).toFixed(3)
+    rightConvert = (userInput.value / kgToLb).toFixed(3)
+    conversion = `${userInput.value} kilograms = ${leftConvert} pounds| ${userInput.value} pounds = ${rightConvert} kilograms`
+    mass.textContent = conversion
 }
 
-convertBtn.addEventListener('click', lengthConvert())
+function renderConversion() {
+    getLength()
+    getVolume()
+    getMass()
+}
+
+convertBtn.addEventListener('click', renderConversion)
