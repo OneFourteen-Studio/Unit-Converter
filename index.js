b231ae6c3ae88902e61ec44485e32b7162678828
OneFourteen Studio <onefourteenstudio114@gmail.com>
2023-08-02 15:47:17 -0400

    adjusting sizing

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function getVolume() {
 function getMass() {
     leftConvert = (userInput.value * kgToLb).toFixed(3)
     rightConvert = (userInput.value / kgToLb).toFixed(3)
-    conversion = `${userInput.value} kilograms = ${leftConvert} pounds | ${userInput.value} pounds = ${rightConvert} kilograms`
+    conversion = `${userInput.value} kilos = ${leftConvert} pounds | ${userInput.value} pounds = ${rightConvert} kilos`
     mass.textContent = conversion
 }
 
